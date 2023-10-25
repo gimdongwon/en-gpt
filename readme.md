50d2ffdf6d8b6334e285b7ebc63c1d9cbb6c2ad7
gimdongwon <ehddnjs8989@gmail.com>
2023-10-25 19:31:50 +0900

    (feat) first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# en-gpt
+
+## 목표
+
+`open ai의 api를 이용해 bitbucket의 코드리뷰를 완성하라`
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const { PORT } = process.env;
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+const server = app.listen(PORT, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("Server is working : PORT - ", port);
+});
