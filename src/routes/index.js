993c7f560fca48e07c4e171bd3c7433301849448
gimdongwon <ehddnjs8989@gmail.com>
2023-10-30 02:29:27 +0900

    (feat) openai gpt call 성공

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const { callChatGpt } = require("../services");
 
 router.get("/", async (req, res) => {
-  return res.status(200).json({
-    message: "success",
-  });
+  // return res.status(200).json({
+  //   message: "success",
+  // });
+  const prompt = "안녕 gpt야, 널 api 테스트 중이야";
+  const result = await callChatGpt(prompt);
+  if (result) {
+    res.status(200).json({
+      result,
+    });
+  } else {
+    res.status(500).json({ error: "api error" });
+  }
 });
 
 module.exports = router;
